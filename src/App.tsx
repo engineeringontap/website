b19c013ad9ebb0eb88cbb8273d5fad0dfd78458d
Peter Mösenthin <moesenthin@sipgate.de>
2019-04-18 18:37:51 +0200

    update to latest event

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ class App extends Component {
         </header>
         <div className={styles.dates}>
           <ul className={styles.list}>
-            <li className={styles.listItem}>15. März 2019, mit Golo Roden</li>
-            <li className={styles.listItem}>18. April 2019, How to become a better software engineer</li>
+            <li className={styles.listItem}>18. April 2019, How to become a better software engineer by doing nothing</li>
             <li className={styles.listItem}>17. Mai 2019, Going Serverless with Knative</li>
             <li className={styles.listItem}>21. Juni 2019, to be announced</li>
             <li className={styles.listItem}>19. Juli 2019, to be announced</li>
