6263762c9f4bfc636957b6e55a6e4c15bf465df5
Peter Mösenthin <moesenthin@sipgate.de>
2019-05-17 15:52:56 +0200

    fix: maps link

--- src/components/EventLocation/EventLocation.tsx
+++ src/components/EventLocation/EventLocation.tsx
@@ -5,7 +5,10 @@ import hafenquelle from "../../assets/hafenquelle_logo.png";
 
 export const EventLocation: React.FC<any> = () => {
 	const openMaps = () => {
-		window.open("https://goo.gl/maps/QWaYYxmsZvd7G6FH8", "_blank");
+		window.open(
+			"https://www.google.com/maps/place/Hafenquelle/@51.2120741,6.7534954,17z/data=!3m1!4b1!4m5!3m4!1s0x47b8ca73995c0769:0x1977ed1227dfe0c5!8m2!3d51.2120741!4d6.7556841",
+			"_blank"
+		);
 	};
 	return (
 		<div className={styles.root}>
