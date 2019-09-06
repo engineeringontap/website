6af4e6b632a487aca0f7b57704c5aae5108a780c
Peter Mösenthin <peter.moesenthin@gmail.com>
2019-09-06 15:17:45 +0200

    chore: add jeopardy

--- src/components/Dates/Dates.tsx
+++ src/components/Dates/Dates.tsx
@@ -8,7 +8,7 @@ export const Dates: React.FC = () => {
 				Next events
 			</h2>
 			<ul className={styles.list}>
-				<li className={styles.listItem}>Do 17. Oktober 2019, to be announced</li>
+				<li className={styles.listItem}>Do 17. Oktober 2019, Jeopardy!</li>
 			</ul>
 			<p>
 				Got something to talk about?
