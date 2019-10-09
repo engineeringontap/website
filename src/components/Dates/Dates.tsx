4fa55a855fa08e3d00569dcec4d8417d21a6f2cd
Peter Mösenthin <peter.moesenthin@gmail.com>
2019-10-09 11:20:11 +0200

    add subtext

--- src/components/Dates/Dates.tsx
+++ src/components/Dates/Dates.tsx
@@ -8,7 +8,15 @@ export const Dates: React.FC = () => {
 				Next events
 			</h2>
 			<ul className={styles.list}>
-				<li className={styles.listItem}>Do 17. Oktober 2019, Jeopardy!</li>
+				<li className={styles.listItem}>
+					<span>Do 17. Oktober 2019, Jeopardy!</span>
+					<span className={styles.listItemSubText}>
+						Nach der Sommerpause starten wir wieder voll durch, mit dem besten Spaß, den du je
+						gehabt haben wirst. Und zwar kostenlos! Wir treffen uns wieder in der Hafenquelle um
+						diverse Biers, Colas und Snacks zu vertilgen, Jeopardy zu spielen und ordentlich
+						abzunerden.
+					</span>
+				</li>
 			</ul>
 			<p>
 				Got something to talk about?
@@ -16,6 +24,7 @@ export const Dates: React.FC = () => {
 				<a
 					className={styles.link}
 					target="_blank"
+					rel="noopener noreferrer"
 					href="https://docs.google.com/forms/d/e/1FAIpQLScOA0ED0faOoeUwLef9c1nBFhAytATTx_q_i5mrn7PS73fciA/viewform?usp=pp_url"
 				>
 					Submit your idea!
